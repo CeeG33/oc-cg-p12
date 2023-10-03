8253f891ba3652e39794b55a822311822ed94706
CeeG33 <ciran.gurbuz@gmail.com>
2023-10-03 18:24:04 +0300

    Create_db file + test rearrangement

---
+++ epicevents/create_db.py
@@ -0,0 +1,30 @@
+from data_access_layer.database import psql_db
+from data_access_layer.client import Client
+from data_access_layer.collaborator import Collaborator
+from data_access_layer.company import Company
+from data_access_layer.contract import Contract
+from data_access_layer.department import Department
+from data_access_layer.event import Event
+
+"""Connecting to the database"""
+psql_db.connect()
+
+"""Table creation"""
+psql_db.create_tables([Client, Collaborator, Company, Contract, Department, Event])
+
+Department.create(name="Management")
+Department.create(name="Sales")
+Department.create(name="Support")
+
+Collaborator.create(
+    identity="Admin",
+    email="admin@epicevents.com",
+    password="admin",
+    department=1
+)
+
+# Collaborator.delete_by_id(6)
+
+
+"""Closing the database"""
+psql_db.close()
--- tests/test_data_access_layer/unit_tests/test_collaborator/test_creation.py
+++ tests/test_data_access_layer/unit_tests/test_collaborator/test_creation.py
@@ -1,43 +1,47 @@
 import pytest
-from epicevents.data_access_layer import collaborator
+from epicevents.data_access_layer.collaborator import Collaborator
 
 def test_collaborator_creation_successfully():
     identity = "Utilisateur Test"
     email = "test@epicevents.com"
+    password = "password"
     department = 1
     
-    test_collaborator = collaborator.create_user(identity, email, department)
+    test_collaborator = Collaborator.create(identity=identity, email=email, password=password, department=department)
     
     assert test_collaborator.identity == identity
     assert test_collaborator.email == email
-    assert test_collaborator.department == department
+    assert test_collaborator.department.id == department
     
 def test_collaborator_creation_with_wrong_identity():
     identity = "56465565"
     email = "test@epicevents.com"
+    password = "password"
     department = 1
     
     with pytest.raises(ValueError):
-        collaborator.create_user(identity, email, department)
+        Collaborator.create(identity=identity, email=email, password=password, department=department)
     
 def test_collaborator_creation_with_wrong_email():
     identity = "Utilisateur Test"
     email = "56465565"
+    password = "password"
     department = 1
     
     with pytest.raises(ValueError):
-        collaborator.create_user(identity, email, department)
+        Collaborator.create(identity=identity, email=email, password=password, department=department)
 
 def test_collaborator_creation_with_wrong_id():
     identity = "Utilisateur Test"
     email = "test@epicevents.com"
+    password = "password"
     department = 154644
     
     with pytest.raises(ValueError):
-        collaborator.create_user(identity, email, department)
+        Collaborator.create(identity=identity, email=email, password=password, department=department)
 
 def test_collaborator_creation_with_missing_attribute():
     with pytest.raises(ValueError):
-        collaborator.create_user()
+        Collaborator.create()
 
 
