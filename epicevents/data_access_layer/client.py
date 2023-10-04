110b20a1514083f15a8cf897a6d17822be788a2b
CeeG33 <ciran.gurbuz@gmail.com>
2023-10-04 18:56:49 +0300

    Client model & tests improvement

--- epicevents/data_access_layer/client.py
+++ epicevents/data_access_layer/client.py
@@ -1,4 +1,5 @@
-from datetime import date
+import re
+from datetime import date, datetime, timedelta
 from peewee import *
 from .database import BaseModel
 from .company import Company
@@ -14,6 +15,30 @@ class Client(BaseModel):
     last_update = DateField()
     collaborator = ForeignKeyField(Collaborator, backref="associated_sales")
     
-    # def save(self, *args, **kwargs):
-
-    #     super().save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        if not self.identity:
+            raise ValueError("Erreur : Vous n'avez pas renseigné les détails du client.")
+        self._validate_identity()
+        self._validate_email()
+        self.identity.capitalize()
+        super().save(*args, **kwargs)
+        
+    def _validate_identity(self):
+        pattern = r'^[a-zA-ZÀ-ÿ-]+ [a-zA-ZÀ-ÿ-]+$'
+        if not re.match(pattern, self.identity):
+            raise ValueError("Erreur : Veuillez entrer une identité correcte (Exemple : Alain Terieur)")
+        
+    def _validate_email(self):
+        pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        if not re.match(pattern, self.email):
+            raise ValueError("Erreur : Veuillez entrer un email valide (Exemple : alain.terieur@epicevents.com)")
+        
+    # def get_data(self):
+    #     client_data = {
+    #         "client_id" : f"{self.id}",
+    #         "email": f"{self.email}",
+    #         "company_id": f"{self.company}",
+    #         "collaborator_id": f"{self.collaborator}",
+    #     }
+    
+    #     return client_data
---
+++ tests/test_data_access_layer/unit_tests/test_client/test_creation.py
@@ -0,0 +1,137 @@
+import pytest
+from datetime import datetime, timedelta, date
+from peewee import IntegrityError, DataError
+from epicevents.data_access_layer.client import Client
+
+def test_client_creation():
+    identity = "Client Test"
+    email = "test@client.fr"
+    phone = "0654987852"
+    company = 1
+    creation_date = date.today()
+    last_update = None
+    collaborator = 2
+    
+    client = Client(
+        identity=identity,
+        email=email,
+        phone=phone,
+        company=company,
+        creation_date=creation_date,
+        last_update=last_update,
+        collaborator=collaborator
+    )
+    
+    assert client.identity == identity
+    assert client.email == email
+    assert client.phone == phone
+    assert client.company.id == company
+    assert client.creation_date == creation_date
+    assert client.last_update == last_update
+    assert client.collaborator.id == collaborator
+    
+def test_client_creation_with_wrong_identity():
+    identity = "465456"
+    email = "test@client.fr"
+    phone = "0654987852"
+    company = 1
+    creation_date = date.today()
+    last_update = None
+    collaborator = 2
+    
+    with pytest.raises(ValueError):
+        Client.create(
+        identity=identity,
+        email=email,
+        phone=phone,
+        company=company,
+        creation_date=creation_date,
+        last_update=last_update,
+        collaborator=collaborator
+        )
+    
+def test_client_creation_with_wrong_email():
+    identity = "Client Test"
+    email = "5465465"
+    phone = "0654987852"
+    company = 1
+    creation_date = date.today()
+    last_update = None
+    collaborator = 2
+    
+    with pytest.raises(ValueError):
+        Client.create(
+        identity=identity,
+        email=email,
+        phone=phone,
+        company=company,
+        creation_date=creation_date,
+        last_update=last_update,
+        collaborator=collaborator
+        )
+
+def test_client_creation_with_wrong_company_id():
+    identity = "Client Test"
+    email = "test@client.fr"
+    phone = "0654987852"
+    company = "Error"
+    creation_date = date.today()
+    last_update = None
+    collaborator = 2
+    
+    with pytest.raises(DataError):
+        Client.create(
+        identity=identity,
+        email=email,
+        phone=phone,
+        company=company,
+        creation_date=creation_date,
+        last_update=last_update,
+        collaborator=collaborator
+        )
+
+def test_client_creation_with_wrong_creation_date():
+    identity = "Client Test"
+    email = "test@client.fr"
+    phone = "0654987852"
+    company = 1
+    creation_date = "Error"
+    last_update = None
+    collaborator = 2
+    
+    with pytest.raises(DataError):
+        Client.create(
+        identity=identity,
+        email=email,
+        phone=phone,
+        company=company,
+        creation_date=creation_date,
+        last_update=last_update,
+        collaborator=collaborator
+        )
+
+def test_client_creation_with_wrong_collaborator_id():
+    identity = "Client Test"
+    email = "test@client.fr"
+    phone = "0654987852"
+    company = 1
+    creation_date = date.today()
+    last_update = None
+    collaborator = "Wrong"
+    
+    with pytest.raises(DataError):
+        Client.create(
+        identity=identity,
+        email=email,
+        phone=phone,
+        company=company,
+        creation_date=creation_date,
+        last_update=last_update,
+        collaborator=collaborator
+        )
+
+def test_collaborator_creation_with_missing_attribute():
+    with pytest.raises(ValueError):
+        Client.create()
+        
+
